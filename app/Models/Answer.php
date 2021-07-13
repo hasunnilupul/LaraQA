14524724892a2dc2d918f940ad60adef46829fa7
Hasun Nilupul <hasunnilupul16@gmail.com>
2021-07-13 14:24:59 +0530

    Update best_answer_id on questions table when when an answer is removed

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -45,18 +45,27 @@ class Answer extends Model
     {
         parent::boot();
 
-        static::created(function ($answer){
+        static::created(function ($answer) {
             $answer->question->increment('answers_count');
         });
 
-        static::deleted(function ($answer)
-        {
+        static::deleted(function ($answer) {
+            $question = $answer->question;
             $answer->question->decrement('answers_count');
+            if ($question->best_answer_id == $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id == $this->question->best_answer_id ? 'accepted':'not-accepted';
     }
 }
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-@props(['type','title','message'])
+@props(['type', 'title', 'message'])
 
 @php
 $typeClass = '';
@@ -20,31 +20,34 @@ switch ($type) {
 @endphp
 
 <div x-data="{isShow: true}">
-    <div x-show="isShow" class="w-full mb-2" 
-        x-transition:enter="transition transform ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-y-0" 
-        x-transition:enter-end="opacity-100 translate-y-1"
-        x-transition:leave="transition transform ease-in duration-300"
-        x-transition:leave-start="opacity-100 translate-y-1" 
-        x-transition:leave-end="opacity-0 translate-y-0">
-        <div class="flex items-center shadow-lg rounded-md space-x-2 bg-white border-gray-100 border px-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 h-6 w-6 bg-white {{ $typeClass }}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                @if ($type=='info')
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    <div x-show="isShow" class="w-full mb-2">
+        <div class="flex items-center shadow-lg rounded-md space-x-2 bg-white border-gray-100 border px-3"
+            x-transition:enter="transition transform ease-out duration-300"
+            x-transition:enter-start="opacity-0 translate-y-0" x-transition:enter-end="opacity-100 translate-y-1"
+            x-transition:leave="transition transform ease-in duration-300"
+            x-transition:leave-start="opacity-100 translate-y-1" x-transition:leave-end="opacity-0 translate-y-0">
+            <svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 h-6 w-6 bg-white {{ $typeClass }}"
+                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                @if ($type == 'info')
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 @elseif ($type=='warning')
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                 @elseif ($type=='error')
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
                 @elseif ($type=='success')
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 @endif
             </svg>
-            
+
             <div class="flex-1 space-y-1 py-2">
-                @if($title)
+                @if ($title)
                     <p class="text-base leading-6 font-semibold antialiased {{ $typeClass }}">{{ $title }}</p>
                 @endif
-                @if($message)
+                @if ($message)
                     <p class="text-sm leading-5 text-gray-400 antialiased">{{ $message }}</p>
                 @endif
             </div>
