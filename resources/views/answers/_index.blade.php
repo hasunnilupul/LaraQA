4e1e81e2540d09969914af0028972fa23f8d5ea4
Hasun Nilupul <hasunnilupul16@gmail.com>
2021-07-15 02:36:16 +0530

    Styling questions and answers

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -3,96 +3,11 @@
         <h2 class="text-lg font-bold text-gray-600">
             {{ $answersCount . ' ' . Str::plural('Answer', $answersCount) }}</h2>
         <hr class="border-gray-300">
-        @foreach ($answers as $answer)
-            <div class="flex justify-start items-start mx-2 py-2 text-gray-600 border-b border-gray-300">
-                <div class="flex flex-col justify-center items-center py-1">
-                    <a title="This answer is usefull" href="#"
-                        class="block vote-active {{ Auth::guest() ? 'off' : '' }}"
-                        onclick="event.preventDefault();document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-up-fill w-8 h-8"
-                            viewBox="0 0 16 16">
-                            <path
-                                d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                        </svg>
-                    </a>
-                    <form action="/answers/{{ $answer->id }}/vote" method="post"
-                        id="up-vote-answer-{{ $answer->id }}" class="hidden">
-                        @csrf
-                        <input type="hidden" name="vote" value="1" />
-                    </form>
-
-                    <span class="block text-lg font-bold py-2">{{ $answer->votes_count }}</span>
-
-                    <a title="This answer is not usefull" href="#" class="block vote {{ Auth::guest() ? 'off' : '' }}"
-                        onclick="event.preventDefault();document.getElementById('down-vote-answer-{{ $answer->id }}').submit();">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            class="bi bi-caret-down-fill w-8 h-8" viewBox="0 0 16 16">
-                            <path
-                                d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                        </svg>
-                    </a>
-                    <form action="/answers/{{ $answer->id }}/vote" method="post"
-                        id="down-vote-answer-{{ $answer->id }}" class="hidden">
-                        @csrf
-                        <input type="hidden" name="vote" value="-1" />
-                    </form>
-
-                    @can('accept', $answer)
-                        <a title="Mark this answer as best answer" href="#"
-                            class="block mt-2 text-center {{ $answer->status }}"
-                            onclick="event.preventDefault();document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-lg h-5 w-5"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
-                            </svg>
-                        </a>
-                    @else
-                        @if ($answer->is_best)
-                            <a title="Marked as the best answer by answer owner" href="#"
-                                class="block mt-2 text-center {{ $answer->status }}" onclick="event.preventDefault();">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-lg h-5 w-5"
-                                    viewBox="0 0 16 16">
-                                    <path
-                                        d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z" />
-                                </svg>
-                            </a>
-                        @endif
-                    @endcan
-                </div>
-                <div class="flex flex-grow flex-col justify-center items-start pl-3">
-                    {!! $answer->body_html !!}
-                    <div class="flex flex-auto w-full justify-between items-center mt-1">
-                        <div class="flex flex-auto">
-                            @can('update', $answer)
-                                <a href="{{ route('questions.answers.edit', [$answer->id, $answer->id]) }}"
-                                    class="px-2 py-1 mr-2 border border-blue-500 text-gray-600 rounded text-sm font-semibold hover:bg-blue-500 hover:text-gray-100">
-                                    Edit
-                                </a>
-                            @endcan
-                            @can('delete', $answer)
-                                <form action="{{ route('questions.answers.destroy', [$answer->id, $answer->id]) }}"
-                                    method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit"
-                                        class="px-2 py-1 border border-red-500 text-gray-600 rounded text-sm font-semibold hover:bg-red-500 hover:text-gray-100"
-                                        onclick="return confirm('Are you sure?')">
-                                        Delete
-                                    </button>
-                                </form>
-                            @endcan
-                        </div>
-                        <div class="flex flex-col justify-end">
-                            @include('shared._author', [
-                            'model' => $answer,
-                            'label' => 'answered'
-                            ])
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
+        <div class="answers-list">
+            @foreach ($answers as $answer)
+                @include('answers._answer')
+            @endforeach
+        </div>
     @endif
 
     @include('answers._create')
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,63 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-4 lg:px-5">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-sm">
                 @forelse ($questions as $question)
-                    <div
-                        class="sm:flex sm:items-center sm:justify-between p-4 border-b border-gray-200 hover:bg-gray-50">
-                        <div class="flex flex-col flex-grow-0 sm:w-18 items-center justify-center sm:pr-4">
-                            <div class="flex flex-col w-full items-center justify-center px-1 mb-2">
-                                <strong class="text-md font-bold text-gray-600">{{ $question->votes_count }}</strong>
-                                <span
-                                    class="text-xs font-light text-gray-500">{{ Str::plural('vote', $question->votes_count) }}</span>
-                            </div>
-                            <div
-                                class="flex flex-col w-full items-center justify-center px-1 mb-2 {{ $question->status }}">
-                                <strong class="text-md font-bold text-gray-600">{{ $question->answers_count }}</strong>
-                                <span
-                                    class="text-xs font-light text-gray-500">{{ Str::plural('answer', $question->answers_count) }}</span>
-                            </div>
-                            <div class="flex w-full items-center justify-center px-1">
-                                <strong
-                                    class="text-xs font-light text-gray-600">{{ $question->views . ' ' . Str::plural('view', $question->views) }}</strong>
-                            </div>
-                        </div>
-                        <div class="flex-1 min-w-0 sm:border-l-2 sm:border-gray-200 sm:pl-2">
-                            <div class="flex items-center">
-                                <h2 class="text-md font-bold leading-7 sm:text-lg sm:truncate">
-                                    <a href="{{ $question->url }}"
-                                        class="text-gray-600 hover:text-gray-800 hover:underline">{{ $question->title }}</a>
-                                </h2>
-                                <div class="flex ml-auto">
-                                    @can('update', $question)
-                                        <a href="{{ route('questions.edit', $question->id) }}"
-                                            class="px-2 py-1 mr-2 border border-blue-500 text-gray-600 rounded text-sm font-semibold hover:bg-blue-500 hover:text-gray-100">
-                                            Edit
-                                        </a>
-                                    @endcan
-                                    @can('delete', $question)
-                                        <form action="{{ route('questions.destroy', $question->id) }}" method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit"
-                                                class="px-2 py-1 border border-red-500 text-gray-600 rounded text-sm font-semibold hover:bg-red-500 hover:text-gray-100"
-                                                onclick="return confirm('Are you sure?')">
-                                                Delete
-                                            </button>
-                                        </form>
-                                    @endcan
-                                </div>
-                            </div>
-                            <div class="flex items-baseline mt-1 sm:mt-0 text-sm text-gray-500">
-                                Asked by
-                                <a href="{{ $question->user->url }}"
-                                    class="font-semibold text-blue-400 hover:text-blue-600 px-1 hover:underline">{{ $question->user->name }}</a>
-                                on
-                                <small class="text-gray-500 px-1">{{ $question->created_date }}</small>
-                            </div>
-                            <p class="text-sm text-gray-500 mt-2">
-                                {{ $question->excerpt }}
-                            </p>
-                        </div>
-                    </div>
+                    @include('questions._excerpt')
                 @empty
                     <div class="sm:flex sm:items-center sm:justify-start p-4 border-b bg-yellow-200 text-yellow-800 border-yellow-200">
                         <strong class="pr-1">Sorry</strong>there are no questions available.    
